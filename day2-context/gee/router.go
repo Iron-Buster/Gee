2ce8724148f855678dfb6c8832f4ab74068ed5af
meilicat <1457480465@qq.com>
2024-09-14 09:56:21 +0800

    update router.go

--- day2-context/gee/router.go
+++ day2-context/gee/router.go
@@ -9,16 +9,19 @@ type router struct {
 	handlers map[string]HandlerFunc
 }
 
+// 新建一个router
 func newRouter() *router {
 	return &router{handlers: make(map[string]HandlerFunc)}
 }
 
+// 添加路由，绑定path和handler的关系
 func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
 	log.Printf("Route %4s - %s", method, pattern)
 	key := method + "-" + pattern
 	r.handlers[key] = handler
 }
 
+// 处理请求，接受一个上下文对象
 func (r *router) handle(c *Context) {
 	key := c.Method + "-" + c.Path
 	if handler, ok := r.handlers[key]; ok {
